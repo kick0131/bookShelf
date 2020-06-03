d39f96074ec5d99ce094fe4381862f1334f368d8
HiramatsuChihiro <hiramatsu.chr@ncontr.com>
2020-06-03 19:53:53 +0900

    Reactのテストコードを追加

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,28 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
+import renderer from 'react-test-renderer';
+import RegistBook from './context/regist/RegistBook';
+import Header from './context/layout/Header';
 
 test('renders learn react link', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
+  const linkElement = getByText(/Writtened by/i);
   expect(linkElement).toBeInTheDocument();
+});
+
+// スナップショットテスト
+test('RegistBook SnapShot test', () => {
+  const tree = renderer
+    .create(<RegistBook />)
+    .toJSON();
+  expect(tree).toMatchSnapshot();
+});
+
+// モック動作確認
+jest.mock('./context/layout/Header', () => () => 'SomeComponent');
+test('Header use mock', () => {
+  const tree = renderer
+    .create(<Header />);
+  expect(tree.toJSON()).toEqual('SomeComponent');
 });
